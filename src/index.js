10c1e35f4d9957244b1ba477d0ee33f2bcff22c8
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-09-12 13:47:01 +0300

    api-1

--- src/index.js
+++ src/index.js
@@ -7,22 +7,7 @@ import { onErrorNotification } from './pnotify';
 onErrorNotification("test");
 
 
-// function attractionId() {
-//     fetch(
-//         "https://app.ticketmaster.com/discovery/v2/attractions?apikey=AmacJHw1PVxi43hxMLwa56XAbBAafJvj&locale=*",
-//     )
-//         .then(response => response.json())
-//         .then(data => console.log(data._embedded.attractions[0]));
-// }
-// attractionId();
-// function eventSearch(keyWord) {
-//     fetch(
-//         "https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=${keyWord}&apikey=AmacJHw1PVxi43hxMLwa56XAbBAafJvj",
-//     )
-//         .then(response => response.json())
-//         .then(data => console.log(data._embedded.events));
-// }
-// eventSearch('Ukraine');
+
 function venueSearch(key) {
     fetch(
         `https://app.ticketmaster.com/discovery/v2/venues.json?apikey=AmacJHw1PVxi43hxMLwa56XAbBAafJvj&countryCode=${key}`,
