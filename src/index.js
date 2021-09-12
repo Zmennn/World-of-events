7cea1ddb2cbb35d34194b1f421f6e5e4a8c29cdf
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-09-12 13:46:22 +0300

    test-api

--- src/index.js
+++ src/index.js
@@ -4,4 +4,31 @@ import './sass/main.scss';
 
 //Импорт и вызов нотифашки, образец
 import { onErrorNotification } from './pnotify';
-onErrorNotification("test")
+onErrorNotification("test");
+
+
+// function attractionId() {
+//     fetch(
+//         "https://app.ticketmaster.com/discovery/v2/attractions?apikey=AmacJHw1PVxi43hxMLwa56XAbBAafJvj&locale=*",
+//     )
+//         .then(response => response.json())
+//         .then(data => console.log(data._embedded.attractions[0]));
+// }
+// attractionId();
+// function eventSearch(keyWord) {
+//     fetch(
+//         "https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=${keyWord}&apikey=AmacJHw1PVxi43hxMLwa56XAbBAafJvj",
+//     )
+//         .then(response => response.json())
+//         .then(data => console.log(data._embedded.events));
+// }
+// eventSearch('Ukraine');
+function venueSearch(key) {
+    fetch(
+        `https://app.ticketmaster.com/discovery/v2/venues.json?apikey=AmacJHw1PVxi43hxMLwa56XAbBAafJvj&countryCode=${key}`,
+    )
+        .then(response => response.json())
+        .then(data => console.log(data._embedded.venues));
+}
+let e = venueSearch('Uk');
+console.log(e, 'www');
