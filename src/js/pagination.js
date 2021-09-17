449ba4b8d225979d007f304f4a905f531f578eb4
RitaDyvnych <m.dyvnych@gmail.com>
2021-09-17 15:41:57 +0300

    полностью реализована пагинация

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,4 @@
-import shortPagTpl from '../templates/shortPagination.hbs'
-import longPagTpl from '../templates/longPagination.hbs'
+import paginationTpl from '../templates/pagination.hbs'
 
 const refs = {
     output: document.querySelector('.js-pagination'),
@@ -17,89 +16,17 @@ export default function pagination(pages) {
     first.classList.add('pagination__item--current');
 }
 
-
-function paginationLong(pages) {
-    for (let i = 0; i < 5; i++){
-        array[i] = i + 1;
-    }
-    array[5] = '...';
-    array[6] = pages;
-    refs.output.innerHTML = longPagTpl({ array: array });
-    links = document.querySelectorAll('.pagination__link');
-    for (let item of links) {        
-        if (item.textContent === '...') {
-            item.classList.add('pagination__link--dots');
-            item.classList.remove('pagination__link');
-        }
-    }
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', onClick);
-    }
-    
-    
-}
-
 function paginationShort(pages) {
     for (let i = 0; i < pages; i++){
         array[i] = i + 1;
     }
-
-    refs.output.innerHTML = shortPagTpl(array);
+    refs.output.innerHTML = paginationTpl(array);
     links = document.querySelectorAll('.pagination__link');
     for (let i = 0; i < links.length; i++) {
         links[i].addEventListener('click', onClickShort);
     }
 
 }
-
-function onClick(e) {
-    e.preventDefault();
-    const first = document.querySelector('.pagination__link');
-    first.classList.remove('pagination__item--current');
-    prevPage = currPage;
-    currPage = e.target.textContent;
-
-    // console.dir(links[6].textContent);
-
-    if (parseInt(currPage) > 4) {
-        array = [1, '...', 4, 5, 6, '...', links[6].textContent];
-        refs.output.innerHTML = longPagTpl({ array: array });
-        links = document.querySelectorAll('.pagination__link');
-        for (let item of links) {        
-        if (item.textContent === '...') {
-            item.classList.add('pagination__link--dots');
-            item.classList.remove('pagination__link');
-        }
-    }
-    }
-
-
-    // for (let i = 0; i < links.length; i++){
-    //     if (i = 5) {
-    //         array = [1, '...', 4, 5, 6, '...', links[6].textContent];
-    //         refs.output.innerHTML = longPagTpl({ array: array });
-    //     }
-    // }
-
-
-
-
-
-    for (let item of links) {        
-        if (item.textContent === prevPage) {
-            item.classList.remove('pagination__item--current');
-        }
-    }
-    for (let item of links) {        
-        if (item.textContent === currPage) {
-            item.classList.add('pagination__item--current');
-        }
-    }
-  
-    console.log('current page:', currPage);
-    return currPage;
-}
-
 function onClickShort(e) {
     e.preventDefault();
     const first = document.querySelector('.pagination__link');
@@ -122,4 +49,70 @@ function onClickShort(e) {
     return currPage;
 }
 
-pagination(17);
+function paginationLong(pages) {
+    array = [1, 2, 3, 4, 5, '...', pages];
+    refs.output.innerHTML = paginationTpl(array);
+    refs.output.addEventListener('click', onClick);
+    links = document.querySelectorAll('.pagination__link');
+    for (let item of links) {        
+        if (item.textContent === '...') {
+            item.classList.add('pagination__link--dots');
+        }
+    }
+    
+}
+function onClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('pagination__link')) {
+        return;
+    }
+    const first = document.querySelector('.pagination__link');
+    first.classList.remove('pagination__item--current');
+    prevPage = currPage;
+    currPage = e.target.textContent;
+
+    if (parseInt(currPage) <= 4) {
+        array = [1, 2, 3, 4, 5, '...', parseInt(links[6].textContent)];
+        longPaginationMarkup(array, links, currPage, prevPage);
+        console.log('current page:', currPage);
+        return currPage;
+    }
+
+    if ((parseInt(currPage) > 4) && (parseInt(currPage) <= (parseInt(links[6].textContent) - 4))) {
+        array = [1, '...', parseInt(currPage) - 1, currPage, parseInt(currPage) + 1, '...', parseInt(links[6].textContent)];
+        longPaginationMarkup(array, links, currPage, prevPage);
+        console.log('current page:', currPage);
+        return currPage;
+    }
+
+    if (parseInt(currPage) > (parseInt(links[6].textContent) - 4)) {
+        array = [1, '...', parseInt(links[6].textContent) - 4, parseInt(links[6].textContent) - 3,
+            parseInt(links[6].textContent) - 2, parseInt(links[6].textContent) - 1, parseInt(links[6].textContent)];
+        longPaginationMarkup(array, links, currPage, prevPage);
+        console.log('current page:', currPage);
+        return currPage;
+    }
+}
+function longPaginationMarkup(array, links, currPage, prevPage) {
+    refs.output.innerHTML = paginationTpl(array);
+        links = document.querySelectorAll('.pagination__link');
+        for (let item of links) {
+            if (item.textContent === '...') {
+                item.classList.add('pagination__link--dots');
+                item.classList.remove('pagination__link');
+            }
+        }
+
+        for (let item of links) {
+            if (item.textContent === prevPage) {
+                item.classList.remove('pagination__item--current');
+            }
+        }
+        for (let item of links) {
+            if (item.textContent === currPage) {
+                item.classList.add('pagination__item--current');
+            }
+        }
+}
+
+pagination(13);
