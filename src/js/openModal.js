73188470b8093a39c20206fdc834c398745190ae
Paul-Semenko <paul.semenko.ps@gmail.com>
2021-09-18 02:37:07 +0300

    cделал закрытие моделки на оверлей и по клавише esc

--- src/js/openModal.js
+++ src/js/openModal.js
@@ -2,37 +2,47 @@ import getRefs from './get-refs';
 import eventsGrid from '../templates/events-grid.hbs';
 
 const modalRefs = {
-openModal: document.querySelector ('.modal-container'),    
-picture: document.querySelector ('.event-grid'),
-closeModal: document.querySelector('.btn-close'),
-overlay: document.querySelector('.js-overlay'),
+    openModal: document.querySelector('.modal-container'),
+    picture: document.querySelector('.event-grid'),
+    closeModal: document.querySelector('.btn-close'),
+    overlay: document.querySelector('.js-overlay'),
 }
 
 
-modalRefs.picture.addEventListener ('click', onModalOpen);
+modalRefs.picture.addEventListener('click', onModalOpen);
 modalRefs.closeModal.addEventListener('click', onModalClose);
 
 function onModalOpen(e) {
-e.preventDefault();
+    e.preventDefault();
 
-if(!e.target.classList.contains('card__img') && 
-   !e.target.classList.contains('card') &&
-   !e.target.classList.contains('card__heading') &&
-   !e.target.classList.contains('card__date') &&
-   !e.target.classList.contains('card_place')
-   ) {
-    return
-}
-onOverlay();
-modalRefs.openModal.classList.add('open-modal');
+    if (!e.target.classList.contains('card__img') &&
+        !e.target.classList.contains('card') &&
+        !e.target.classList.contains('card__heading') &&
+        !e.target.classList.contains('card__date') &&
+        !e.target.classList.contains('card_place')
+    ) {
+        return
+    }
+    onOverlay();
+    modalRefs.openModal.classList.add('open-modal');
 }
 
 function onModalClose(e) {
-modalRefs.overlay.classList.remove('overlay');    
-modalRefs.openModal.classList.remove('open-modal');
+    modalRefs.overlay.classList.remove('overlay');
+    modalRefs.openModal.classList.remove('open-modal');
 
 }
 
-function onOverlay (e) {
-modalRefs.overlay.classList.add('overlay'); 
+function onOverlay(e) {
+    modalRefs.overlay.classList.add('overlay');
+}
+
+window.addEventListener('keyup', modalCloseESC);
+modalRefs.overlay.addEventListener('click', onModalClose);
+
+function modalCloseESC(e) {
+    if (e.key !== 'Escape') {
+        return;
+    }
+    onModalClose();
 }
